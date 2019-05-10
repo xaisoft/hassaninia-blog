4519fae04f011f36ded4f46a4f081bd750ff1b33
shassaninia <shassaninia@ORLWS052>
2019-05-10 09:05:59 -0400

    remove underline from link

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,19 +3,37 @@ import { Link } from "gatsby"
 
 const Menu = () => {
   return (
-    <div style={{paddingTop: "10px" }}>
+    <div style={{ paddingTop: "10px" }}>
       <ul
         style={{
           listStyle: "none",
           display: "flex",
-          justifyContent: "space-evenly"
+          justifyContent: "space-evenly",
         }}
       >
-        <li style={{border:'1px solid purple',padding:'10px'}}>
-          <Link to="/" style={{textDecoration:'none'}}>Home</Link>
+        <li style={{ border: "1px solid purple", padding: "10px" }}>
+          <Link
+            to="/"
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+          >
+            Home
+          </Link>
         </li>
-        <li style={{border:'1px solid purple',padding:'10px'}}>
-          <Link to="/" style={{textDecoration:'none'}}>Blog</Link>
+        <li style={{ border: "1px solid purple", padding: "10px" }}>
+          <Link
+            to="/"
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+          >
+            Blog
+          </Link>
         </li>
       </ul>
     </div>
