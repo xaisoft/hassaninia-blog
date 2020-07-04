f0309de55cf0098858a78a08348aeec8afe2dac0
Saied Hassaninia <xaisoft80@gmail.com>
2020-07-04 14:38:53 -0400

    Updated packages

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -62,7 +62,7 @@ const Layout = props => {
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}
-        <a href="https://www.hassaninia.com">Hassaninia</a>
+        <a href="https://www.hassaninia.com">&amp;Hassaninia</a>
       </footer>
     </div>
   )
